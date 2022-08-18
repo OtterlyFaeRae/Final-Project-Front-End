f933a00c3a17de111f8f5a396a906d4afef0c389
TarekApps <mizzy887x@gmail.com>
2022-08-18 16:52:27 +0100

    added some utils

--- trading-app/src/components/Navbar.js
+++ trading-app/src/components/Navbar.js
@@ -7,16 +7,16 @@ const Navbar = () => {
           <Link to='/'>Landing</Link>
         </li>
         <li>
-          <Link to='/Login'>Login</Link>
+          <Link to='/login'>Login</Link>
         </li>
         <li>
-          <Link to='/Buy'>Buy</Link>
+          <Link to='/buy'>Buy</Link>
         </li>
         <li>
-          <Link to='/Sell'>Sell</Link>
+          <Link to='/sell'>Sell</Link>
         </li>
         <li>
-          <Link to='/Portfolio'>Portfolio</Link>
+          <Link to='/portfolio'>Portfolio</Link>
         </li>
       </ul>
     </nav>
---
+++ trading-app/src/utils/index.js
@@ -0,0 +1,69 @@
+export const signUp = async (username, email, password, setter) => {
+    try {
+        const response = await fetch("http://localhost:5000/user", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              username: username,
+              email: email,
+              password: password,
+            }),
+          });
+          const data = await response.json()
+          console.log(data);
+    } catch (error) {
+        console.log(error) 
+    }
+}
+
+export const login = async (username, password, setter) => {
+    try {
+      const response = await fetch("http://localhost:5000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  export const getToken = async () => {
+    try {
+        const response = await fetch("http://localhost:5000/login", {
+            method: "GET",
+            'Authorization': token
+          });
+          const data = await response.json()
+          console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  export const updateUser = async (username, email, password)
+
+  export const deleteUser = async (username, password, setter) => {
+    try {
+      const response = await fetch("http://localhost:5000/user", {
+        headers: { "Content-Type": "application/json" },
+        method: "DELETE",
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+
+      const data = await response.json();
+      console.log(data.user);
+      console.log("User Deleted");
+
+    }catch (error) {
+        console.log(error)
+    }
+}

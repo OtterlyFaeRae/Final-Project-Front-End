281b14c98e83bf6d1ef211875b08ed42b6a51f52
ShaunBillows <billows.shaun@gmail.com>
2022-08-19 19:42:36 +0100

    portfilio formatting

--- trading-app/src/pages/Portfolio.js
+++ trading-app/src/pages/Portfolio.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import Navbar from "../components/Navbar"
 import PortfolioItem from "../components/PortfolioItem"
 import { useState } from "react"
+import styled from "styled-components";
 
 function Portfolio({ setIsLoggedIn, isLoggedIn, user }) {
 
@@ -12,42 +13,55 @@ function Portfolio({ setIsLoggedIn, isLoggedIn, user }) {
 	}, [user])
 	
 	return (
-		<div>
+		<Cont>
 			<Navbar 
 				setIsLoggedIn={setIsLoggedIn} 
 				isLoggedIn={isLoggedIn} 
 			/>
 			<h2>This is the Portfolio Page</h2>
-			<table>
-				<tr>
-					<th>
-						name 
-					</th>
-					<th>
-						symbol
-					</th>
-					<th>
-						price
-					</th>
-					<th>
-						quantity
-					</th>
-				</tr>
-				{
-					stocks
-					&&
-					stocks.map( (stock, i) => (
-						<PortfolioItem 
-							name={stock[0].name} 
-							symbol={stock[0].symbol} 
-							price={""} 
-							number={stock[0].number}
-						/>
-					))
-				}
-			</table>
-		</div>
+			<TableCont>
+				<table>
+					<thead>
+						<tr>
+							<th>
+								name 
+							</th>
+							<th>
+								symbol
+							</th>
+							<th>
+								price
+							</th>
+							<th>
+								quantity
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+					{
+						stocks
+						&&
+						stocks.map( (stock, i) => (
+							<PortfolioItem 
+								name={stock[0].name} 
+								symbol={stock[0].symbol} 
+								price={""} 
+								number={stock[0].number}
+								key={i}
+							/>
+						))
+					}
+					</tbody>
+				</table>
+			</TableCont>
+		</Cont>
 	);
 }
 
 export default Portfolio;
+
+const Cont = styled.div`
+`
+const TableCont = styled.div`
+
+`

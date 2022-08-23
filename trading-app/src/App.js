2333b9312d6cc024af87a2bc6b393e1eeb5925b2
ShaunBillows <billows.shaun@gmail.com>
2022-08-23 13:44:35 +0100

    font changed to roboto

--- trading-app/src/App.js
+++ trading-app/src/App.js
@@ -160,7 +160,7 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     padding: 0;
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    font-family: 'Roboto', 'Oxygen',
 	'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
 	sans-serif;
 	// dev tool

f73d593bab486528d0342c27507a0f15b3277035
Fitz8 <liamfitzgerald@hotmail.co.uk>
2022-08-24 15:12:59 +0100

    Buy page styling

--- trading-app/src/components/BuyStock.js
+++ trading-app/src/components/BuyStock.js
@@ -1,198 +1,226 @@
 import { useState } from "react";
-import styled from "styled-components"
-import { useNavigate } from 'react-router-dom'
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 import { addStocks, updateCash } from "../utils";
-import Modal from 'react-modal';
-import "../Modal.css"
-import noMoney from "../images/no money.png"
-import yesMoney from "../images/cashInHand.png"
+import Modal from "react-modal";
+import "../Modal.css";
+import noMoney from "../images/no money.png";
+import yesMoney from "../images/cashInHand.png";
 
 const BuyStock = ({ price, stockToBuy, user, cookies, setUser }) => {
+  const [input, setInput] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
+  const [purchaseSuccesful, setPurchaseSuccesful] = useState(false);
 
-    const [ input, setInput ] = useState("");
-    const [modalOpen, setModalOpen] = useState(false);
-    const [purchaseSuccesful, setPurchaseSuccesful] = useState(false);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleOnChange = async (e) => {
+    setInput(e.target.value);
+  };
 
-    const handleOnChange = async e => {
-        setInput(e.target.value);
-    };
-
-    const handleKeyDown = async e => {
-        if (e.key === "Enter") {
-            await buyStock();
-        };
-    };
-
-	const handleClick = async () => {
-        await buyStock();
-    };
-
-    const buyStock = async () => {
-        if (input) {
-            const total = price * input
-            if (total > user.cash) {
-                isModalOpen();
-            } else {
-                await addStocks( stockToBuy, "stock name", parseInt(input), cookies, setUser)
-                await updateCash( user.cash - total, setUser, cookies )
-                setPurchaseSuccesful(true)
-                isModalOpen()
-            };
-            setInput("");
-        };
-    };
-
-    const refreshPage = () => {
-        window.location.reload(false);
-    };
-
-    //MODAL FUNCTIONS
-    const closeModalUnsuccessful = () => {
-        closeModal()
-        setInput("")
+  const handleKeyDown = async (e) => {
+    if (e.key === "Enter") {
+      await buyStock();
     }
+  };
 
-    const closeModalSuccessful = () => {
-        navigate("/portfolio")
-        refreshPage()
+  const handleClick = async () => {
+    await buyStock();
+  };
+
+  const buyStock = async () => {
+    if (input) {
+      const total = price * input;
+      if (total > user.cash) {
+        isModalOpen();
+      } else {
+        await addStocks(
+          stockToBuy,
+          "stock name",
+          parseInt(input),
+          cookies,
+          setUser
+        );
+        await updateCash(user.cash - total, setUser, cookies);
+        setPurchaseSuccesful(true);
+        isModalOpen();
+      }
+      setInput("");
     }
+  };
 
-    const isModalOpen = () => {
-        setModalOpen(true)
-    }
+  const refreshPage = () => {
+    window.location.reload(false);
+  };
 
-    const closeModal = () => {
-        setModalOpen(false)
-    }
+  //MODAL FUNCTIONS
+  const closeModalUnsuccessful = () => {
+    closeModal();
+    setInput("");
+  };
 
-    Modal.setAppElement('#root');
+  const closeModalSuccessful = () => {
+    navigate("/portfolio");
+    refreshPage();
+  };
 
-    const customStyles = {
-        content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: "auto",
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-            background: "#222224"
-        },
-    };
-    return (
-        <Cont>
-        {
-            stockToBuy !== "No stock found."
-            ?
-            <StockCont>
-                <TopCont>
-                        <TopSubCont>
-                            <SymbolCont>
-                                <p>Symbol:</p>
-                                <SymbolBox>{stockToBuy}</SymbolBox>
-                            </SymbolCont>
-                            <OwnedStockBox>
-                                <p>Your stocks:</p>
-                                <p>{user.stocks.find(x => x.name === stockToBuy) ?  user.stocks.find(x => x.name === stockToBuy).number : 'None.'}</p>
-                            </OwnedStockBox>
-                        </TopSubCont>
-                            <ValBox>
-                                <h2>${Math.round(price*100)/100}</h2>
-                            </ValBox>
-                    </TopCont>
-                <BottomCont>
-                    <input type="number" placeholder="Quantity" onKeyDown={handleKeyDown} onChange={handleOnChange} value={input} />
-                    <button onClick={handleClick} placeholder="buy">Buy</button>
-                </BottomCont>
-            </StockCont>
-            :
-            <StockNotFoundCont>
-                <p>No stock found.</p>
-            </StockNotFoundCont>
-        }
-        <Modal
-        isOpen={modalOpen}
-        style={customStyles}
-        closeTimeoutMS={200}
-        >
+  const isModalOpen = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  Modal.setAppElement("#root");
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      background: "#222224",
+    },
+  };
+  return (
+    <Cont>
+      {stockToBuy !== "No stock found." ? (
+        <StockCont>
+          <h2>{stockToBuy}</h2>
+          <p>Quantity of stock owned:&nbsp;
+            {user.stocks.find((x) => x.name === stockToBuy)
+              ? user.stocks.find((x) => x.name === stockToBuy).number
+              : "None."}
+          </p>
+          <p>Price per stock: ${Math.round(price * 100) / 100}</p>
+          <BottomCont>
+            <Input
+              type="number"
+              placeholder="Quantity"
+              onKeyDown={handleKeyDown}
+              onChange={handleOnChange}
+              value={input}
+            />
+            <Button2 onClick={handleClick} placeholder="buy">
+              Buy
+            </Button2>
+          </BottomCont>
+        </StockCont>
+      ) : (
+        <StockNotFoundCont>
+          <p>No stock found.</p>
+        </StockNotFoundCont>
+      )}
+      <Modal isOpen={modalOpen} style={customStyles} closeTimeoutMS={200}>
+        <div>
+          {!purchaseSuccesful ? (
             <div>
-                { !purchaseSuccesful
-                ?
-                <div>
-                    <h1 className="Insufficent">INSUFFICIENT FUNDS</h1>
+              <h1 className="Insufficent">INSUFFICIENT FUNDS</h1>
 
-                    <img className="noMoney" src={noMoney} alt=""/>
+              <img className="noMoney" src={noMoney} alt="" />
 
-                    <h2 className="valid-ammount">PLEASE ENTER A VALID AMMOUNT</h2>
-                    <button className="closeUnsuccessful" onClick={closeModalUnsuccessful}>CLOSE</button>
-                </div>
-                :
-                <div>
-                    <h1 className="Insufficent">PURCHASE SUCCESSFUL</h1>
-
-                    <img className="yesMoney" src={yesMoney} alt=""/>
-
-                    <button className="closeSuccessful" onClick={closeModalSuccessful}>CLOSE</button>
-                </div>
-                };
+              <h2 className="valid-ammount">PLEASE ENTER A VALID AMOUNT</h2>
+              <button
+                className="closeUnsuccessful"
+                onClick={closeModalUnsuccessful}
+              >
+                CLOSE
+              </button>
             </div>
-        </Modal>
+          ) : (
+            <div>
+              <h1 className="Insufficent">PURCHASE SUCCESSFUL</h1>
+
+              <img className="yesMoney" src={yesMoney} alt="" />
+
+              <button
+                className="closeSuccessful"
+                onClick={closeModalSuccessful}
+              >
+                CLOSE
+              </button>
+            </div>
+          )}
+          ;
+        </div>
+      </Modal>
     </Cont>
-    );
+  );
 };
 
-export default BuyStock
+export default BuyStock;
 
 const Cont = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
 
 const StockCont = styled.div`
-    display: flex;
-    flex-direction: column;
-    border: 2px solid #31356E;
-    background: black;
-    min-height: 150px;
-    min-width: 300px;
-    padding-top: 1%;
-    padding-left: 1%;
-    padding-right: 1%;
-`
-
-const TopCont = styled.div`
-    display:flex;
-    padding-bottom: 30px;
-`
-
-const TopSubCont = styled.div`
-    display:flex;
-    flex-direction: column; 
-`
-
-const SymbolCont = styled.div`
-	display: flex;
-    padding-bottom: 10px;
-`
-
-const SymbolBox = styled.p`
-    padding-left: 5px;
-`
-
-const OwnedStockBox = styled.div`
-    display:flex;
-`
-
-const ValBox = styled.div`
-    padding-left: 25%;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  justify-content: center;
+  min-height: 150px;
+  min-width: 300px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  margin-top: 50px;
+`;
 
 const BottomCont = styled.div`
-	display: flex;
-`
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+min-width: 400px;
+gap: 20px;
+`;
+
 const StockNotFoundCont = styled.div`
-    margin: 4rem 7rem;
+  margin: 4rem 7rem;
+`;
+
+const Button2 = styled.button`
+  background: #5e5df0;
+  margin-top: 5px;
+  border-radius: 12px;
+  box-sizing: border-box;
+  color: #ffffff;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 24px;
+  opacity: 1;
+  outline: 0 solid transparent;
+  padding: 8px 28px;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: fit-content;
+  word-break: break-word;
+  border: 0;
 `
+
+const Input = styled.input`
+  border-style: solid;
+  border-color: white;
+  border-width: 1px;
+  background-color: #212121;
+  border-radius: 5px;
+  padding: 5px;
+  font-size: 20px;
+  outline: none;
+  caret-color: white;
+  color: white;
+  max-width: 120px;
+  margin-top: 7px;
+  &:focus {
+    border-color: #5e5df0;
+  }
+`;
--- trading-app/src/pages/Buy.js
+++ trading-app/src/pages/Buy.js
@@ -1,64 +1,65 @@
 import { useState } from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 // import DropdownItem from "../components/DropdownItem";
-import Navbar from "../components/Navbar"
-import BuyStock from "../components/BuyStock"
+import Navbar from "../components/Navbar";
+import BuyStock from "../components/BuyStock";
 import { getPrices } from "../utils/finnhub-fetch";
+import background from "../images/stocks3.jpg";
 
 function Buy({ setIsLoggedIn, isLoggedIn, user, cookies, setUser, logOut }) {
+  const [input, setInput] = useState("");
+  const [stockToBuy, SetStockToBuy] = useState("");
+  const [price, setPrice] = useState("");
 
-	const [ input, setInput ] = useState("");
-	const [ stockToBuy, SetStockToBuy ] = useState("");
-	const [ price, setPrice ] = useState("");
+  const handleOnChange = async (e) => {
+    setInput(e.target.value);
+  };
 
-	const handleClickClear = () => {
-        setInput("");
-    };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && input) {
+      searchStock();
+    }
+  };
 
-	const handleOnChange = async e => {
-        setInput(e.target.value);
-    };
+  const handleClickSearch = async () => {
+    if (input) {
+      searchStock();
+    }
+  };
 
-    const handleKeyDown = e => {
-        if (e.key === "Enter" && input) {
-			searchStock();
-        };
-    };
+  const searchStock = async () => {
+    const buyPrice = await getPrices([input]);
+    SetStockToBuy(() => input);
+    if (!buyPrice) {
+      SetStockToBuy(() => "No stock found.");
+    }
+    setPrice(buyPrice);
+    setInput("");
+  };
 
-	const handleClickSearch = async () => {
-		if (input) {
-			searchStock();
-		};
-    };
-
-	const searchStock = async () => {
-		const buyPrice = await getPrices([input]);
-		SetStockToBuy( () => input );
-		if (!buyPrice) {
-			SetStockToBuy( () => "No stock found.");
-		};
-		setPrice( buyPrice );
-        setInput("");
-	};
-
-	return (
-		<>
-			<Navbar 
-				setIsLoggedIn={setIsLoggedIn}
-				isLoggedIn={isLoggedIn} 
-				logOut={logOut}
-				user={user}
-			/>
-			<Cont>
-				<h2>Buy Stocks</h2>
-				<br/>
-				<p>Your Cash: ${user && user.cash.toFixed(2)}</p>
-				<SearchCont>
-					<Input type="text" placeholder="Search stocks..." onKeyDown={handleKeyDown} onChange={handleOnChange} value={input}></Input>
-					<ClearInput onClick={handleClickClear}>X</ClearInput>
-					<SearchButton onClick={handleClickSearch}>Search</SearchButton>
-				</SearchCont>
-				{/* <DropdownCont>
+  return (
+    <Cont background={background}>
+      <Navbar
+        setIsLoggedIn={setIsLoggedIn}
+        isLoggedIn={isLoggedIn}
+        logOut={logOut}
+        user={user}
+      />
+      <Content>
+        <h2>Buy Stocks</h2>
+        <p>Your Cash: ${user && user.cash.toFixed(2)}</p>
+        <SearchCont>
+          <Input
+            type="text"
+            placeholder="Search stocks..."
+            onKeyDown={handleKeyDown}
+            onChange={handleOnChange}
+            value={input}
+          ></Input>
+          {/* <ClearInput onClick={handleClickClear}>X</ClearInput> */}
+          <SearchButton onClick={handleClickSearch}>Search</SearchButton>
+        </SearchCont>
+        {/* <DropdownCont>
 					<DropdownList>
 						{
 							input 
@@ -69,87 +70,101 @@ function Buy({ setIsLoggedIn, isLoggedIn, user, cookies, setUser, logOut }) {
 						}
 					</DropdownList>
 				</DropdownCont> */}
-				{
-					stockToBuy 
-					&&
-					<BuyStock price={price} stockToBuy={stockToBuy} user={user} cookies={cookies} setUser={setUser}/>
-				}
-			</Cont>
-		</>
-	);
-};
+        {stockToBuy && (
+          <BuyStock
+            price={price}
+            stockToBuy={stockToBuy}
+            user={user}
+            cookies={cookies}
+            setUser={setUser}
+          />
+        )}
+      </Content>
+    </Cont>
+  );
+}
 
 export default Buy;
 
 const Cont = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	padding-top: 5%;
-`
-const SearchCont = styled.div`
-	margin: 1rem;
-	width: 300px;
-    height: 40px;
-	margin-top: 5%;
-	position: relative;
-`
-const Input = styled.input`
-	width: 100%;
-	height: 100%;
-	border: solid #31356E 2px;
-	border-radius: 10px;
-	position: absolute;
-	font-size: medium;
-	background-color: black;
-	color: white;
-	padding: 8px 10px 8px 35px;
-	&:focus {
-		outline: none;
-		border-color: #483d8b;
-    	box-shadow: 0 0 10px #3d428a;
-	}
-	&:hover {
-		outline: none;
-    border-color: #483d8b;
-    box-shadow: 0 0 10px #3d428a;
-	}
-`
-const SearchButton = styled.button`
-	position: absolute;
-	height: 36px;
-	border-radius: 6px;
-	right: 0;
-	background-color: #3d428a;
-	&:hover {
-		background-color: #3d428a;
-		outline: solid #3d428a 1px;
-		box-shadow: 0 0 25px #31356E;
-	}
-	border: none;
-	cursor: pointer;
-	color: rgba(255,255,255,0.7);
-	font-size: medium;
-	padding: 0 12px;
-	margin: 2px;
-`
-const ClearInput = styled.button`
-	background-color: black;
-	border: none;
-	color: rgba(255,255,255,0.55);
-	&:hover {
-		color: rgba(255,255,255,0.45);
-	}
-	position: absolute;
-	z-index: 3;
-    position: absolute;
-    top: 9px;
-    left: 10px;
-	font-size: large;
-	cursor: pointer;
-	padding-left: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3em;
+  height: 100vh;
+  background-image: url(${(props) => props.background});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  gap: 100px;
+`;
 
-`
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 50px;
+  padding-bottom: 50px;
+  padding-right: 100px;
+  padding-left: 100px;
+  border-radius: 10px;
+  border: solid;
+  background-color: #212121;
+  border-color: #5e5df0;
+  border-width: 1.5px;
+  opacity: 1;
+  gap: 20px;
+  max-width: 75%;
+`;
+
+const SearchCont = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-evenly;
+align-items: center;
+min-width: 400px;
+`;
+
+const Input = styled.input`
+  border-style: solid;
+  border-color: white;
+  border-width: 1px;
+  background-color: #212121;
+  border-radius: 5px;
+  padding: 5px;
+  font-size: 20px;
+  outline: none;
+  caret-color: white;
+  color: white;
+  max-width: 220px;
+  margin-top: 7px;
+  &:focus {
+    border-color: #5e5df0;
+  }
+`;
+
+const SearchButton = styled.button`
+  background: #5e5df0;
+  margin-top: 5px;
+  border-radius: 12px;
+  box-sizing: border-box;
+  color: #ffffff;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 24px;
+  opacity: 1;
+  outline: 0 solid transparent;
+  padding: 8px 28px;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: fit-content;
+  word-break: break-word;
+  border: 0;
+`;
+
 // const DropdownCont = styled.div`
 // `
 // const DropdownList = styled.ul`

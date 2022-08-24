9eaae566d7d2a7b1a769a839f3560f5fec038d70
OtterlyFaeRae <Moonturtledsc@Gmail.com>
2022-08-24 12:36:04 +0100

    Cookies removed from dependency array App

--- trading-app/src/App.js
+++ trading-app/src/App.js
@@ -32,7 +32,7 @@ function App() {
 
 	useEffect( (cookies, setCookie) => {
 		checkToken(cookies, setCookie, setUser, setIsLoggedIn)
-	}, [cookies, setCookie]);
+	}, []);
 
 	// ------------------------------------------------------------------- //
 	// state altering functions
